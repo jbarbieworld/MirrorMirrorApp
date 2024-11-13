c2c7eb9a46c103c712b9c912a27f07c8e568b670
f-marilin <f.marilin@wustl.edu>
2024-11-12 20:11:44 -0600

    ignore: collection view swipe

---
+++ MirrorMirror/MirrorMirror/ClothingCell.swift
@@ -0,0 +1,49 @@
+//
+//  ClothingCell.swift
+//  MirrorMirror
+//
+//IGNORE THIS IS FOR COLLECTION VIEW SWIPE
+//
+//  Created by Marilin Francisco on 11/11/24.
+//
+
+
+
+/*
+
+import Foundation
+import UIKit
+
+class ClothingCell: UICollectionViewCell{
+    
+    
+    @IBOutlet weak var topImageView: UIImageView!
+    @IBOutlet weak var bottomImageView: UIImageView!
+    @IBOutlet weak var accessoryImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Any additional setup after loading from nib, if using nibs.
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        // Initialize your image views programmatically
+        topImageView = UIImageView()
+        bottomImageView = UIImageView()
+        accessoryImageView = UIImageView()
+        
+        // Add subviews
+        contentView.addSubview(topImageView)
+        contentView.addSubview(bottomImageView)
+        contentView.addSubview(accessoryImageView)
+        
+        // Set up layout or constraints here if necessary
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+*/
